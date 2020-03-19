742606aec77f67d26236db4ce177b498f77c8181
Vadim  Ghedreutan <vadimghedreutan@MacBook-Pro-15.local>
2020-03-20 00:51:52 +0100

    added social links

--- src/components/header.js
+++ src/components/header.js
@@ -4,9 +4,15 @@ const Header = () => {
   return (
     <div className="header">
       <div className="container">
-        <div className="row v-center">
+        <div className="row v-center space-between">
           <div className="logo">
             <a href="/">Travel warnings</a>
+          </div>
+          <div>
+            <ul className="social">
+              <li><a href="https://github.com/vadimghedreutan"><i className="fab fa-github"></i></a></li>
+              <li><a href="https://twitter.com/GhedreutanVadim"><i className="fab fa-twitter"></i></a></li>
+            </ul>
           </div>
         </div>
       </div>
